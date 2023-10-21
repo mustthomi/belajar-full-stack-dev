7ea535373b852ee877da77f637e2ad77dc2e2c34
mustthomi <148241830+mustthomi@users.noreply.github.com>
2023-10-21 16:23:30 +0000

    add connection db

--- database/index.js
+++ database/index.js
@@ -1,12 +1,30 @@
 const express = require('express');
-const db = require('./dbconfig');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { Client } = require('pg');
+
+const client = new Client({
+  user: 'postgres',
+  password: 'postgres',
+  host: 'localhost',
+  port: 5432, // default Postgres port
+  database: 'postgres'
+});
 
 const PORT = 3000;
+
 const app = express();
+app.use(cors());
+
+client.connect(err => {
+  if(err) throw err
+  console.log('database conected ...') 
+});
 
 app.get('/', async (req, res) => {
   try {
-    const result = await db.query('SELECT * FROM teachers');
+    const result = await client.query('SELECT * FROM teachers');
+    console.log(result.rows);
     res.json(result.rows);
   } catch (err) {
     console.error(err);
